469a737e743bdafcb2c0cea80ceaeaf8983ccf75
jaewoong <ys05059@gmail.com>
2025-07-30 14:17:53 +0900

    test: vercel ci/cd 테스트

--- frontend/src/app/login/callback/naver/page.tsx
+++
@@ -1,9 +0,0 @@
-import { NaverLoginHandler } from "@/features/auth/social-login/ui/NaverLoginHandler";
-
-export default function NaverCallbackPage() {
-  return (
-    <div className="flex min-h-screen items-center justify-center">
-      <NaverLoginHandler />
-    </div>
-  );
-}
