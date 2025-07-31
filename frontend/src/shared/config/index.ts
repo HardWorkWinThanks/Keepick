dca417af517e205d240081a5e83175f02e6e7e40
jaewoong <ys05059@gmail.com>
2025-07-31 09:55:38 +0900

    update: 서버주소

--- frontend/src/shared/config/index.ts
+++ frontend/src/shared/config/index.ts
@@ -4,4 +4,5 @@ export { Providers } from "./Providers";
 export { default as StoreProvider } from "./StoreProvider";
 export { default as QueryProvider } from "./QueryProvider";
 export { makeStore } from "./store";
-export const SOCKET_SERVER_URL = process.env.NEXT_PUBLIC_SOCKET_SERVER_URL
+export const SOCKET_SERVER_URL =
+  process.env.NEXT_PUBLIC_SOCKET_SERVER_URL || "https://i13d207.p.ssafy.io";
