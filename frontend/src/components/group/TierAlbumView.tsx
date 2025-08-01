131058f6010716f0ad00f555f68de202bcd4b9bc
jaewoong <ys05059@gmail.com>
2025-08-01 09:54:22 +0900

    update: 웃긴짤 넣기

--- frontend/src/components/group/TierAlbumView.tsx
+++ frontend/src/components/group/TierAlbumView.tsx
@@ -98,6 +98,14 @@ export default function TierAlbumView({
           { id: "photo2", src: "/ssafy-dummy2.jpg", name: "사진2" },
           { id: "photo3", src: "/ssafy-dummy3.jpg", name: "사진3" },
           { id: "photo4", src: "/ssafy-dummy4.jpg", name: "사진4" },
+          { id: "photo5", src: "/dummy/dummy1.jpg", name: "사진5" },
+          { id: "photo6", src: "/dummy/dummy2.jpg", name: "사진6" },
+          { id: "photo7", src: "/dummy/dummy3.jpg", name: "사진7" },
+          { id: "photo8", src: "/dummy/dummy4.jpg", name: "사진8" },
+          { id: "photo9", src: "/dummy/dummy5.jpg", name: "사진9" },
+          { id: "photo9", src: "/dummy/dummy6.jpg", name: "사진10" },
+          { id: "photo9", src: "/dummy/dummy7.jpg", name: "사진11" },
+          { id: "photo9", src: "/dummy/dummy8.jpg", name: "사진12" },
         ]);
       }
     } catch (error) {
