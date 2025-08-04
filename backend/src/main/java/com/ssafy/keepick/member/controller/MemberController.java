01e0d784e704510f147253855f990d5deb71ef0c
박재완 <wjp1230@naver.com>
2025-08-04 12:56:08 +0900

    Feat: 회원 정보를 조회하는 MemberController 클래스 추가 [S13P11D207-121]

---
+++ backend/src/main/java/com/ssafy/keepick/member/controller/MemberController.java
@@ -0,0 +1,27 @@
+package com.ssafy.keepick.member.controller;
+
+import com.ssafy.keepick.member.application.MemberService;
+import com.ssafy.keepick.member.controller.response.MemberInfoResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/members")
+@RequiredArgsConstructor
+public class MemberController {
+    
+    private final MemberService memberService;
+    
+    /**
+     * 현재 로그인된 사용자의 정보를 조회합니다.
+     * @return 사용자 정보 응답
+     */
+    @GetMapping("/me")
+    public ResponseEntity<MemberInfoResponse> getCurrentMemberInfo() {
+        MemberInfoResponse response = memberService.getCurrentMemberInfo();
+        return ResponseEntity.ok(response);
+    }
+}
