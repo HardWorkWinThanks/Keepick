667d1f77721e3fbf23a4f0b642ffd0061f9d58f1
kwonssshyeon <kwonsh4396@gmail.com>
2025-08-05 09:55:25 +0900

    Feat: 페이징 공통 응답 포맷 작성 [S13P11D207-100]

---
+++ backend/src/main/java/com/ssafy/keepick/global/response/PagingResponse.java
@@ -0,0 +1,32 @@
+package com.ssafy.keepick.global.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+import java.util.function.Function;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class PagingResponse<T> {
+    private final List<T> list;
+    private final boolean hasNext;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private final Integer page;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private final Integer totalPage;
+
+
+    public static <U, T> PagingResponse<T> from(Page<U> data, Function<U, T> converter) {
+        return PagingResponse.<T>builder()
+                .list(data.getContent().stream().map(converter).toList())
+                .page(data.getNumber())
+                .hasNext(data.hasNext())
+                .totalPage(data.getTotalPages())
+                .build();
+    }
+}
