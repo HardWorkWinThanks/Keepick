239a70cb2d86079dddd9b8acf2c995b2ff824303
kwonssshyeon <kwonsh4396@gmail.com>
2025-08-06 00:14:49 +0900

    Fix: PagingResponse 포맷 수정 [S13P11D207-100]

--- backend/src/main/java/com/ssafy/keepick/global/response/PagingResponse.java
+++ backend/src/main/java/com/ssafy/keepick/global/response/PagingResponse.java
@@ -14,19 +14,34 @@ import java.util.function.Function;
 @AllArgsConstructor
 public class PagingResponse<T> {
     private final List<T> list;
-    private final boolean hasNext;
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    private final Integer page;
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    private final Integer totalPage;
+    private final PageInfo pageInfo;
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    public static class PageInfo {
+        private final boolean hasNext;
+        @JsonInclude(JsonInclude.Include.NON_NULL)
+        private final Integer page;
+        @JsonInclude(JsonInclude.Include.NON_NULL)
+        private final Integer size;
+        @JsonInclude(JsonInclude.Include.NON_NULL)
+        private final Integer totalPage;
+        @JsonInclude(JsonInclude.Include.NON_NULL)
+        private final Long totalElement;
+    }
 
 
     public static <U, T> PagingResponse<T> from(Page<U> data, Function<U, T> converter) {
         return PagingResponse.<T>builder()
                 .list(data.getContent().stream().map(converter).toList())
-                .page(data.getNumber())
-                .hasNext(data.hasNext())
-                .totalPage(data.getTotalPages())
+                .pageInfo(PageInfo.builder()
+                        .page(data.getNumber())
+                        .size(data.getSize())
+                        .totalPage(data.getTotalPages())
+                        .totalElement(data.getTotalElements())
+                        .hasNext(data.hasNext())
+                        .build())
                 .build();
     }
 }
