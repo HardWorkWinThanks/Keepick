9c10cd30846cf94c12a5a212b67779c0252bf095
박재완 <wjp1230@naver.com>
2025-08-06 13:56:27 +0900

    Feat: TierAlbumDto 클래스에 필드 추가 및 변환 메서드 구현 [S13P11D207-143]

--- backend/src/main/java/com/ssafy/keepick/album/tier/application/dto/TierAlbumDto.java
+++ backend/src/main/java/com/ssafy/keepick/album/tier/application/dto/TierAlbumDto.java
@@ -1,5 +1,33 @@
 package com.ssafy.keepick.album.tier.application.dto;
 
+import java.time.LocalDateTime;
+
+import com.ssafy.keepick.album.tier.domain.TierAlbum;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
 public class TierAlbumDto {
-    
+    private Long id;
+    private String name;
+    private String description;
+    private String thumbnailUrl;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private Integer photoCount;
+
+
+    public static TierAlbumDto from(TierAlbum tierAlbum) {
+        return TierAlbumDto.builder()
+                .id(tierAlbum.getId())
+                .name(tierAlbum.getName())
+                .description(tierAlbum.getDescription())
+                .thumbnailUrl(tierAlbum.getThumbnailUrl())
+                .createdAt(tierAlbum.getCreatedAt())
+                .updatedAt(tierAlbum.getUpdatedAt())
+                .photoCount(tierAlbum.getPhotoCount())
+                .build();
+    }
 }
