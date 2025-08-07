456a8ab4a1d0043a9072c2027ba3ecce7135e3a2
kwonssshyeon <kwonsh4396@gmail.com>
2025-08-07 13:37:54 +0900

    Docs: 하이라이트 앨범 API swagger 문서 작성 [S13P11D207-149]

---
+++ backend/src/main/java/com/ssafy/keepick/highlight/controller/HighlightAlbumController.java
@@ -0,0 +1,47 @@
+package com.ssafy.keepick.highlight.controller;
+
+import com.ssafy.keepick.global.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/groups/{groupId}/highlight-albums")
+@RequiredArgsConstructor
+@Tag(name="Highlight Album", description = "하이라이트 앨범 관련 API")
+public class HighlightAlbumController {
+
+    @PostMapping("/photos")
+    @Operation(summary = "하이라이트 스크린샷 업로드", description = "화상채팅 중 감정이 감지되어 캡처된 사진의 S3 URL을 전달받아 저장합니다.")
+    public ApiResponse<?> saveHighlightScreenshot(@PathVariable Long groupId) {
+        return ApiResponse.created(null);
+    }
+
+    @PostMapping()
+    @Operation(summary = "하이라이트 앨범 생성", description = """
+            화상채팅 종료 후 감정이 감지된 참여자들의 스크린샷을 기반으로 하이라이트 앨범을 생성합니다.
+            감정이 감지되지 않은 참여자는 앨범에 포함되지 않습니다.
+            """)
+    public ApiResponse<?> createHighlightAlbum(@PathVariable Long groupId) {
+        return ApiResponse.created(null);
+    }
+
+    @PutMapping("{albumId}")
+    @Operation(summary = "하이라이트 앨범 수정", description = """
+            특정 하이라이트 앨범의 제목, 설명을 수정하고, 지정된 사진들을 삭제합니다.
+            새로운 사진 추가는 불가능합니다
+            """)
+    public ApiResponse<?> modifyHighlightAlbum(@PathVariable Long groupId, @PathVariable Long albumId) {
+        return ApiResponse.created(null);
+    }
+
+    @DeleteMapping("{albumId}")
+    @Operation(summary = "하이라이트 앨범 삭제", description = """
+            지정된 하이라이트 앨범을 삭제합니다. 삭제된 앨범은 해당 그룹의 모든 참여자에게 더 이상 표시되지 않으며,
+            앨범에 포함된 사진도 함께 삭제됩니다.
+            """)
+    public ApiResponse<?> deleteHighlightAlbum(@PathVariable Long groupId, @PathVariable Long albumId) {
+        return ApiResponse.created(null);
+    }
+}
