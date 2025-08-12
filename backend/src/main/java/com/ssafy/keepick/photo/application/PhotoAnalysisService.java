8bdb6bc999262139325fa994f9afe590f473c9d8
kwonssshyeon <kwonsh4396@gmail.com>
2025-08-12 10:09:49 +0900

    Feat: 이미지 분석 후 데이터베이스 저장 로직 구현 [S13P11D207-84]

--- backend/src/main/java/com/ssafy/keepick/photo/application/PhotoAnalysisService.java
+++ backend/src/main/java/com/ssafy/keepick/photo/application/PhotoAnalysisService.java
@@ -4,8 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ssafy.keepick.external.redis.RedisService;
 import com.ssafy.keepick.external.visionai.VisionAIService;
-import com.ssafy.keepick.external.visionai.request.BlurDetectionRequest;
-import com.ssafy.keepick.external.visionai.request.FaceTaggingRequest;
+import com.ssafy.keepick.external.visionai.request.CompositeAnalysisRequest;
 import com.ssafy.keepick.external.visionai.request.SimilarGroupingRequest;
 import com.ssafy.keepick.global.exception.BaseException;
 import com.ssafy.keepick.global.exception.ErrorCode;
@@ -14,6 +13,7 @@ import com.ssafy.keepick.group.persistence.GroupMemberRepository;
 import com.ssafy.keepick.member.domain.Member;
 import com.ssafy.keepick.photo.application.dto.JobStatus;
 import com.ssafy.keepick.photo.application.dto.PhotoAnalysisDto;
+import com.ssafy.keepick.photo.controller.request.PhotoAnalysisRequest;
 import com.ssafy.keepick.photo.domain.Photo;
 import com.ssafy.keepick.photo.domain.PhotoAnalysisJob;
 import com.ssafy.keepick.photo.persistence.PhotoRepository;
@@ -36,48 +36,9 @@ public class PhotoAnalysisService {
     private final GroupMemberRepository groupRepository;
     private final VisionAIService  visionAIService;
     private final RedisService redisService;
+    private final PhotoAnalysisResultService photoAnalysisResultService;
     private final ObjectMapper objectMapper;
 
-    @Async("asyncExecutor")
-    public CompletableFuture<PhotoAnalysisDto> detectBlurPhotos(Long groupId, Long currentMemberId) {
-        // 1. 사용자가 그룹에 속한 사용자가 맞는지 확인
-        groupRepository.findByGroupIdAndMemberId(groupId, currentMemberId)
-                .orElseThrow(() -> new BaseException(ErrorCode.NOT_FOUND));
-
-        // 2. 그룹 갤러리의 삭제되지 않은 모든 사진 조회
-        List<Photo> photos = photoRepository.findByGroupIdAndDeletedAtIsNull(groupId);
-
-        // 3. 분석 요청 request 변환
-        String jobId = UUID.randomUUID().toString();
-        BlurDetectionRequest request = BlurDetectionRequest.from(jobId, photos);
-
-        // 4. 작업 상태 저장
-        PhotoAnalysisJob job = PhotoAnalysisJob.builder()
-                .jobId(jobId)
-                .groupId(groupId)
-                .jobStatus(JobStatus.PENDING)
-                .jobType("blur_detection")
-                .startTime(LocalDateTime.now())
-                .totalImages(photos.size())
-                .processedImages(0)
-                .memberId(currentMemberId)
-                .build();
-        try {
-            String jobJson = objectMapper.writeValueAsString(job);
-            redisService.setValue(jobId, jobJson, Duration.ofHours(1));
-        } catch (JsonProcessingException e) {
-            throw new BaseException(ErrorCode.INTERNAL_VISION_PARSE_ERROR);
-        }
-
-        // 5. vision ai 에 분석 요청
-        visionAIService.postBlurRequest(jobId, request)
-                .subscribe(
-                        response -> log.info("작업 완료 {}", response),
-                        error -> log.error("작업 실패", error)
-                );
-
-        return CompletableFuture.completedFuture(PhotoAnalysisDto.of(jobId, JobStatus.PENDING));
-    }
 
     @Async("asyncExecutor")
     public CompletableFuture<PhotoAnalysisDto> groupingSimilarPhotos(Long groupId, Long currentMemberId) {
@@ -111,9 +72,12 @@ public class PhotoAnalysisService {
         }
 
         // 5. vision ai 에 분석 요청
-        visionAIService.postSimilarityRequest(jobId, request)
+        visionAIService.postSimilarityRequest(request)
                 .subscribe(
-                        response -> log.info("작업 완료 {}", response),
+                        response -> {
+                            log.info("작업 완료 {}", response);
+                            photoAnalysisResultService.saveGroupingResult(response);
+                        },
                         error -> log.error("작업 실패", error)
                 );
 
@@ -121,28 +85,28 @@ public class PhotoAnalysisService {
     }
 
     @Async("asyncExecutor")
-    public CompletableFuture<PhotoAnalysisDto> taggingFaceToPhotos(Long groupId, Long currentMemberId) {
+    public CompletableFuture<PhotoAnalysisDto> analysisCompositePhotos(Long groupId, Long currentMemberId, PhotoAnalysisRequest request) {
         // 1. 사용자가 그룹에 속한 사용자가 맞는지 확인
         groupRepository.findByGroupIdAndMemberId(groupId, currentMemberId)
                 .orElseThrow(() -> new BaseException(ErrorCode.NOT_FOUND));
-
-        // 2-1. 그룹 갤러리의 삭제되지 않은 모든 사진 조회
+        // 2-1. request의 모든 사진 조회
         // 2-2. 그룹의 모든 사용자 조회
-        List<Photo> photos = photoRepository.findByGroupIdAndDeletedAtIsNull(groupId);
+        List<Photo> photos = photoRepository.findAllById(request.getPhotoIds());
+
         List<Member> members = groupRepository.findJoinedMembersById(groupId).stream()
                 .map(GroupMember::getMember)
                 .toList();
 
         // 3. 분석 요청 request 변환
         String jobId = UUID.randomUUID().toString();
-        FaceTaggingRequest request = FaceTaggingRequest.from(jobId, members, photos);
+        CompositeAnalysisRequest compositeAnalysisRequest = CompositeAnalysisRequest.from(jobId, members, photos);
 
         // 4. 작업 상태 저장
         PhotoAnalysisJob job = PhotoAnalysisJob.builder()
                 .jobId(jobId)
                 .groupId(groupId)
                 .jobStatus(JobStatus.PENDING)
-                .jobType("face_tagging")
+                .jobType("composite")
                 .startTime(LocalDateTime.now())
                 .totalImages(photos.size())
                 .processedImages(0)
@@ -156,13 +120,15 @@ public class PhotoAnalysisService {
         }
 
         // 5. vision ai 에 분석 요청
-        visionAIService.postFaceTaggingRequest(jobId, request)
+        visionAIService.postFaceTaggingRequest(compositeAnalysisRequest)
                 .subscribe(
-                        response -> log.info("작업 완료 {}", response),
+                        response -> {
+                            log.info("작업 완료 {}", response.toString());
+                            photoAnalysisResultService.saveAnalysisResult(response);
+                        },
                         error -> log.error("작업 실패", error)
                 );
 
         return CompletableFuture.completedFuture(PhotoAnalysisDto.of(jobId, JobStatus.PENDING));
     }
-
 }
