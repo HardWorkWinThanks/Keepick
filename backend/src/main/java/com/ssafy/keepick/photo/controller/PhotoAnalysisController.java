3d12a76a012f9c8a07e8b3761c83e71ac2c51343
kwonssshyeon <kwonsh4396@gmail.com>
2025-08-12 10:06:39 +0900

    Fix: 이미지 분석 모델 3개 통합 API spec 작성 [S13P11D207-84]

--- backend/src/main/java/com/ssafy/keepick/photo/controller/PhotoAnalysisController.java
+++ backend/src/main/java/com/ssafy/keepick/photo/controller/PhotoAnalysisController.java
@@ -5,6 +5,7 @@ import com.ssafy.keepick.global.security.util.AuthenticationUtil;
 import com.ssafy.keepick.photo.application.PhotoAnalysisService;
 import com.ssafy.keepick.photo.application.PhotoJobProgressService;
 import com.ssafy.keepick.photo.application.dto.PhotoAnalysisDto;
+import com.ssafy.keepick.photo.controller.request.PhotoAnalysisRequest;
 import com.ssafy.keepick.photo.controller.response.PhotoAnalysisJobResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -22,11 +23,12 @@ public class PhotoAnalysisController {
     private final PhotoAnalysisService  photoAnalysisService;
     private final PhotoJobProgressService  photoJobProgressService;
 
-    @PostMapping("/blur")
-    @Operation(summary = "흐린 사진 분류 API", description = "비동기 작업으로 처리되며 작업 id만 우선으로 반환합니다.")
-    public CompletableFuture<ApiResponse<PhotoAnalysisJobResponse>> invokeBlurDetection(@PathVariable Long groupId) {
+    @PostMapping("/composite")
+    @Operation(summary = "YOLO 객체 검출 + 얼굴 매칭 + 흐림 판별 API", description = "비동기 작업으로 처리되며 작업 id만 우선으로 반환합니다.")
+    public CompletableFuture<ApiResponse<PhotoAnalysisJobResponse>> invokeBlurDetection(@PathVariable Long groupId,
+                                                                                        @RequestBody PhotoAnalysisRequest request) {
         Long currentMemberId = AuthenticationUtil.getCurrentUserId();
-        CompletableFuture<PhotoAnalysisDto> result = photoAnalysisService.detectBlurPhotos(groupId, currentMemberId);
+        CompletableFuture<PhotoAnalysisDto> result = photoAnalysisService.analysisCompositePhotos(groupId, currentMemberId, request);
         return CompletableFuture.completedFuture(ApiResponse.ok(PhotoAnalysisJobResponse.from(result)));
     }
 
@@ -38,39 +40,35 @@ public class PhotoAnalysisController {
         return CompletableFuture.completedFuture(ApiResponse.ok(PhotoAnalysisJobResponse.from(result)));
     }
 
-    @PostMapping("/face")
-    @Operation(summary = "사용자 얼굴 식별 API", description = "비동기 작업으로 처리되며 작업 id만 우선으로 반환합니다.")
-    public CompletableFuture<ApiResponse<PhotoAnalysisJobResponse>> invokeFaceTagging(@PathVariable Long groupId) {
-        Long currentMemberId = AuthenticationUtil.getCurrentUserId();
-        CompletableFuture<PhotoAnalysisDto> result = photoAnalysisService.taggingFaceToPhotos(groupId, currentMemberId);
-        return CompletableFuture.completedFuture(ApiResponse.ok(PhotoAnalysisJobResponse.from(result)));
-    }
-
     @GetMapping("/status/{jobId}")
     @Operation(summary = "작업 상태 조회 API", description = """
             비동기적으로 진행되는 이미지 분석 작업의 상태를 조회합니다.
+            
             주기적으로 polling 요청을 하며, 작업이 완료되었을 경우 결과 조회 API를 요청합니다.
+            
+            swagger에서는 테스트가 불가능합니다. POSTMAN등을 이용해주세요.
             """)
-    public SseEmitter getJobStatus(@PathVariable("jobId") String jobId) {
+    public SseEmitter getJobStatus(@PathVariable("groupId") Long groupId,
+                                   @PathVariable("jobId") String jobId) {
         return photoJobProgressService.subscribeToJobStatus(jobId);
     }
 
-    @GetMapping("/blur/{jobId}")
-    @Operation(summary = "흐린 사진 분류 결과 조회를 위한 API")
-    public ApiResponse<?> getBlurJobResult(@PathVariable String jobId) {
-        return ApiResponse.ok(null);
-    }
-
-    @GetMapping("/similarity/{jobId}")
-    @Operation(summary = "유사 사진 분류 결과 조회를 위한 API")
-    public ApiResponse<?> getSimilarityJobResult(@PathVariable String jobId) {
-        return ApiResponse.ok(null);
-    }
-
-    @GetMapping("/face/{jobId}")
-    @Operation(summary = "사용자 얼굴 식별 결과 조회를 위한 API")
-    public ApiResponse<?> getFaceJobResult(@PathVariable String jobId) {
-        return ApiResponse.ok(null);
-    }
+//    @GetMapping("/blur/{jobId}")
+//    @Operation(summary = "흐린 사진 분류 결과 조회를 위한 API")
+//    public ApiResponse<?> getBlurJobResult(@PathVariable String jobId) {
+//        return ApiResponse.ok(null);
+//    }
+//
+//    @GetMapping("/similarity/{jobId}")
+//    @Operation(summary = "유사 사진 분류 결과 조회를 위한 API")
+//    public ApiResponse<?> getSimilarityJobResult(@PathVariable String jobId) {
+//        return ApiResponse.ok(null);
+//    }
+//
+//    @GetMapping("/face/{jobId}")
+//    @Operation(summary = "사용자 얼굴 식별 결과 조회를 위한 API")
+//    public ApiResponse<?> getFaceJobResult(@PathVariable String jobId) {
+//        return ApiResponse.ok(null);
+//    }
 
 }
