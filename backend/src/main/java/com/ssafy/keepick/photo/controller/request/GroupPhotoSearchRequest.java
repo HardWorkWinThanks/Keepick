d1af0b450a5ac67722f8b67bb19402d0c7f146b4
김하은 <siji2135@naver.com>
2025-08-12 13:19:12 +0900

    Fix: 그룹 갤러리 조회 시 페이징 오류 해결

--- backend/src/main/java/com/ssafy/keepick/photo/controller/request/GroupPhotoSearchRequest.java
+++ backend/src/main/java/com/ssafy/keepick/photo/controller/request/GroupPhotoSearchRequest.java
@@ -1,13 +1,16 @@
 package com.ssafy.keepick.photo.controller.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @Getter
+@Setter
 @Builder
 @AllArgsConstructor
 public class GroupPhotoSearchRequest {
@@ -15,8 +18,11 @@ public class GroupPhotoSearchRequest {
     private List<String> tags;
     private LocalDate startDate;
     private LocalDate endDate;
-    private int page;
-    private int size;
+
+    @Schema(description = "페이지 번호", defaultValue = "0")
+    private Integer page;
+    @Schema(description = "페이지 크기", defaultValue = "10")
+    private Integer size;
 
     public GroupPhotoSearchRequest() {
         this.page = 0;
