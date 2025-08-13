ccc06044ced80cec98c5126cab882a357f646794
kwonssshyeon <kwonsh4396@gmail.com>
2025-08-13 09:47:49 +0900

    Docs: swagger response 설명 작성

--- backend/src/main/java/com/ssafy/keepick/photo/controller/response/GroupPhotoUploadResponse.java
+++ backend/src/main/java/com/ssafy/keepick/photo/controller/response/GroupPhotoUploadResponse.java
@@ -2,6 +2,7 @@ package com.ssafy.keepick.photo.controller.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.ssafy.keepick.photo.application.dto.GroupPhotoUploadDto;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 import java.util.regex.Matcher;
@@ -11,8 +12,9 @@ import java.util.regex.Pattern;
 @Builder
 @AllArgsConstructor
 public class GroupPhotoUploadResponse {
+    @Schema(description = "presigned url, not null")
     private String presignedUrl;
-    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @Schema(description = "저장될 사진의 id를 미리 알려줘서 이미지 분석 요청을 할 수 있도록 한다. not null")
     private Long imageId;
 
     public static GroupPhotoUploadResponse from(GroupPhotoUploadDto dto) {
