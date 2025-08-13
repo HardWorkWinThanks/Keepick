d21b7e07c5bc77f3a220dd6854f90b160e8aa30d
kwonssshyeon <kwonsh4396@gmail.com>
2025-08-13 22:12:18 +0900

    Test: 그룹 갤러리 모든 태그 조회 테스트 작성

--- backend/src/test/java/com/ssafy/keepick/photo/persistence/PhotoTagRepositoryTest.java
+++ backend/src/test/java/com/ssafy/keepick/photo/persistence/PhotoTagRepositoryTest.java
@@ -1,5 +1,9 @@
 package com.ssafy.keepick.photo.persistence;
 
+import com.ssafy.keepick.group.domain.Group;
+import com.ssafy.keepick.group.persistence.GroupRepository;
+import com.ssafy.keepick.member.domain.Member;
+import com.ssafy.keepick.member.persistence.MemberRepository;
 import com.ssafy.keepick.photo.domain.Photo;
 import com.ssafy.keepick.photo.domain.PhotoTag;
 import com.ssafy.keepick.support.BaseRepositoryTest;
@@ -15,6 +19,8 @@ class PhotoTagRepositoryTest extends BaseRepositoryTest {
 
     @Autowired PhotoRepository photoRepository;
     @Autowired PhotoTagRepository photoTagRepository;
+    @Autowired GroupRepository groupRepository;
+    @Autowired MemberRepository memberRepository;
 
     @DisplayName("사진의 태그를 조회합니다.")
     @Test
@@ -34,5 +40,36 @@ class PhotoTagRepositoryTest extends BaseRepositoryTest {
         assertThat(photoTags).extracting("tag").containsExactly("태그1", "태그2", "태그3");
     }
 
+    @DisplayName("그룹갤러리의 모든 고유한 태그를 조회합니다.")
+    @Test
+    void findTagsByGroupIdTest() {
+        // given
+        Member member = memberRepository.save(createMember());
+        Group group = groupRepository.save(Group.createGroup("그룹 이름", member));
+        Photo photo = photoRepository.save(Photo.builder().group(group).build());
+
+        photoTagRepository.save(PhotoTag.of(photo, "태그1"));
+        photoTagRepository.save(PhotoTag.of(photo, "태그2"));
+        photoTagRepository.save(PhotoTag.of(photo, "태그3"));
+        photoTagRepository.save(PhotoTag.of(photo, "태그3"));
+
+        // when
+        List<String> photoTags = photoTagRepository.findTagsByGroupId(group.getId());
+
+        // then
+        assertThat(photoTags.size()).isEqualTo(3);
+        assertThat(photoTags).containsExactly("태그1", "태그2", "태그3");
+    }
+
+    Member createMember() {
+        return Member.builder()
+                .name("이름")
+                .email("unique@email.com")
+                .nickname("닉네임")
+                .provider("google")
+                .providerId("123456789")
+                .identificationUrl("http://example.png")
+                .build();
+    }
 
 }
