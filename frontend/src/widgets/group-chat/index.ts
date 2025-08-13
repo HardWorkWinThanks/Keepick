65ad599345c7efe6ef4699c13b8c8ef068918a66
jaewoong <ys05059@gmail.com>
2025-08-13 10:57:32 +0900

    remove: 레거시 파일 제거

--- frontend/src/widgets/group-chat/index.ts
+++
@@ -1,5 +0,0 @@
-// UI 컴포넌트
-export { GroupChatFloatingButton } from "./ui/GroupChatFloatingButton";
-
-// 타입
-export type { GroupChatFloatingButtonProps } from "./ui/types";
--- frontend/src/widgets/group-chat/ui/types.ts
+++
@@ -1,4 +0,0 @@
-export interface GroupChatFloatingButtonProps {
-  groupName: string;
-  isChatActive: boolean;
-}
