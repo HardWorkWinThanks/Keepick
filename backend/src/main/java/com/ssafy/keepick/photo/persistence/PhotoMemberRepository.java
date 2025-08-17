110d7092a8839dbb319fd1c78ee43d79a2f38e73
김하은 <siji2135@naver.com>
2025-08-17 16:44:47 +0900

    Fix: 그룹 갤러리 태그 조회 시 멤버 태그 추가

--- backend/src/main/java/com/ssafy/keepick/photo/persistence/PhotoMemberRepository.java
+++ backend/src/main/java/com/ssafy/keepick/photo/persistence/PhotoMemberRepository.java
@@ -1,13 +1,37 @@
 package com.ssafy.keepick.photo.persistence;
 
+import com.ssafy.keepick.member.domain.Member;
 import com.ssafy.keepick.photo.domain.PhotoMember;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface PhotoMemberRepository extends JpaRepository<PhotoMember, Long> {
 
-    @EntityGraph(attributePaths = {"member"})
-    List<PhotoMember> findAllByPhotoId(Long photoId);
+    @Query("SELECT pm " +
+            "FROM PhotoMember pm " +
+            "JOIN FETCH pm.member m " +
+            "WHERE pm.photo.id = :photoId " +
+            "AND m IN (" +
+            "    SELECT gm.member " +
+            "    FROM GroupMember gm " +
+            "    WHERE gm.group.id = :groupId " +
+            "    AND gm.status = GroupMemberStatus.ACCEPTED" +
+            ")")
+    List<PhotoMember> findAllByPhotoId(Long groupId, Long photoId);
+
+    @Query("SELECT DISTINCT pm.member " +
+            "FROM PhotoMember pm " +
+            "JOIN pm.member m " +
+            "JOIN pm.photo p " +
+            "WHERE p.deletedAt IS NULL " +
+            "AND p.group.id = :groupId " +
+            "AND m IN (" +
+            "    SELECT gm.member " +
+            "    FROM GroupMember gm " +
+            "    WHERE gm.group.id = :groupId " +
+            "    AND gm.status = GroupMemberStatus.ACCEPTED" +
+            ")")
+    List<Member> findMembersByGroupId(Long groupId);
 }
